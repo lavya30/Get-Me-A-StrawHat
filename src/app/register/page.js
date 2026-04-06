8db3bf2cc1ac08076d11580917f4f4c3e3dfd75f
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-04-06 20:16:18 +0530

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -76,7 +76,6 @@ const RegisterPage = () => {
   };
 
   return (
-
     <>
       <Navbar/>
     <div className="min-h-screen bg-transparent relative flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
